3800da1c68fb281803098e4dae261789e7169db8
Maxime Vorwerk <Maxime Vorwerk>
2024-02-28 00:38:51 +0100

    now accepts multiple connections

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include "main.h"
+#include <stdlib.h>
 
 #define PROTOCOL_IP 0
+#define THREAD_MAX 10
+#define CONNECTION_BACKLOG 10
 
 int main (int argc, char *argv[]) {
-    int sockfd, status, yes, connection;
+    int sockfd, status, yes, connection, free_threads;
     struct addrinfo hints, *res, *p, *ipv4;
     socklen_t connection_data_size;
     struct sockaddr connection_data;
+    pthread_t new_connection;
+    struct thread_args *args;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
@@ -40,37 +45,55 @@ int main (int argc, char *argv[]) {
         exit(1);
     }
 
-    if ((status = listen(sockfd, 10)) != 0) {
-        perror("listen");
-        exit(1);
-    }
+    args = malloc(sizeof *args);
+    free_threads = THREAD_MAX;
+    while (1) {
+        if (free_threads == 0) {
+            continue;
+        }
+        if ((status = listen(sockfd, CONNECTION_BACKLOG)) != 0) {
+            perror("listen");
+            exit(1);
+        }
 
-    connection_data_size = sizeof connection_data;
-    if ((connection = accept(sockfd, &connection_data, &connection_data_size)) == -1) {
-        perror("accept");
-        exit(1);
-    }
+        (free_threads)--;
+        fprintf(stdout, "free_threads: %i", free_threads);
 
-    manageConnection(connection, &connection_data, &connection_data_size);
+        connection_data_size = sizeof connection_data;
+        if ((connection = accept(sockfd, &connection_data, &connection_data_size)) == -1) {
+            perror("accept");
+            exit(1);
+        }
+
+        args->connection = connection;
+        args->free_sockets = &free_threads;
+        args->connection_data = connection_data;
+        args->connection_data_size = connection_data_size;
+        
+        pthread_create(&new_connection, NULL, (void *)httpConnection, (void *)args);
+
+    }
     
+    free(args);
     freeaddrinfo(res);
 }
 
-void manageConnection(
-        int connection,
-        struct sockaddr *connection_data,
-        socklen_t *connection_data_size) {
+void *httpConnection(struct thread_args *args) {
     int status, buffer_size;
     char *msg;
 
     buffer_size = 1024;
     msg = malloc(sizeof(char) * buffer_size);
 
-    while ((status = recv(connection, (void *)msg, buffer_size, 0)) > 0) {
+    while ((status = recv(args->connection, (void *)msg, buffer_size, 0)) > 0) {
         fprintf(stdout, "%s\n", msg);
     }
+
+    (args->free_sockets)++;
+
     if (status < 0) {
         perror("recv");
     }
+    return 0;
 }
 
--- main.h
+++ main.h
@@ -4,11 +4,17 @@
 #include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+struct thread_args {
+         int connection;
+        struct sockaddr connection_data;
+        socklen_t connection_data_size;
+        int *free_sockets;
+};
 
 int main(int argc, char *argv[]);
 
-void manageConnection(
-        int connection,
-        struct sockaddr *connection_data,
-        socklen_t *connection_data_size);
+void *httpConnection(struct thread_args *args);
 

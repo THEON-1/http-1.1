be96d567ec00d60227ca1d9fc504ee5097eb4a6d
Maxime Vorwerk <Maxime Vorwerk>
2024-02-28 01:12:17 +0100

    added actually needed basic stack implementation

---
+++ datastructures/stack.c
@@ -0,0 +1,43 @@
+#include "stack.h"
+
+struct stack *stack_initialize() {
+    struct stack *s;
+    s = malloc(sizeof *s);
+    s->head = NULL;
+    s->size = 0;
+    return s;
+}
+
+void stack_push(struct stack *s, void *data) {
+    struct stack_element *e;
+    e = malloc(sizeof *e);
+
+    e->data = data;
+    e->next = s->head;
+    s->head = e;
+    s->size++;
+}
+
+void *stack_pop(struct stack *s) {
+    if (s->size == 0) {
+        fprintf(stderr, "tried to pop empty stack");
+        exit(1);
+    }
+    struct stack_element *e;
+    e = s->head;
+    s->head = e->next;
+    s->size--;
+    return e->data;
+}
+
+void stack_free(struct stack *s) {
+    struct stack_element* e;
+    while (s->size > 0) {
+        e = s->head;
+        s->head = e->next;
+        free(e->data);
+        free(e);
+    }
+    free(s);
+}
+
